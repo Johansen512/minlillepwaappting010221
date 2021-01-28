3517da6c98fac96c2867c4fb418bfeebd318a097
Christian Johansen <137861@RTS-365.dk>
2021-01-28 08:46:39 +0100

    helt nyt og rent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OneSignal from "react-onesignal";
+
+OneSignal.initialize('bdd3a802-1273-48f5-96cb-ea87cd7f691d', {
+  safari_web_id:"web.onesignal.auto.1947bcbb-3df5-45a5-b464-0be0e15f4a2c"
+})
 
 function App() {
   return (
