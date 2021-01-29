e046239d4ebf8a4cb89f67a0282c69843895a709
Christian Johansen <137861@RTS-365.dk>
2021-01-29 12:27:10 +0100

    IOS prompt

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import { Router } from '@reach/router';
 import OneSignal from "react-onesignal";
 import Home from "./components/Home";
 import Page from "./components/Page";
-
+import PWAPrompt from 'react-ios-pwa-prompt'
 OneSignal.initialize('bdd3a802-1273-48f5-96cb-ea87cd7f691d', {
   safari_web_id:"web.onesignal.auto.1947bcbb-3df5-45a5-b464-0be0e15f4a2c"
 })
 
 function App() {
   return (
-    <Router>
+    <div> <Router>
     <Home path= "/" />
     <Page path="/page"/>
     </Router>
+
+<PWAPrompt promptOnVisit={1} timesToShow={3} copyClosePrompt="Close" permanentlyHideOnDismiss={false}/></div>
   );
 }
 
