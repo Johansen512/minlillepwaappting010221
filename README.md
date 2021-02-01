aa30b31213e78e7ee34ded1b0018c9da48e0d229
Christian Johansen <137861@RTS-365.dk>
2021-02-01 09:46:58 +0100

    nyt billed

--- README.md
+++
@@ -1,2 +0,0 @@
-# webappreactserviceworker
-reacts serviceworker
--- src/components/Page.js
+++ src/components/Page.js
@@ -6,7 +6,7 @@ const Page = () => {
         <h2>Dette er undersiden</h2>
 
         <img src={kode} alt="tu" />
-        <img src="http//www.kage.com/kage.jpg" alt="tut" />
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thebalancesmb.com%2Fstart-online-business-with-no-money-4128823&psig=AOvVaw0qWBZ1dT0X8emUp-6gOQM3&ust=1612255477753000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCND85dulyO4CFQAAAAAdAAAAABAN" alt="tut" />
         </div>     );
 }
  
