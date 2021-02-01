65317aa8a7a942b01aa7aab23f1d1dcfff0dc692
Christian Johansen <137861@RTS-365.dk>
2021-02-01 09:19:52 +0100

    med streg

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Router } from '@reach/router';
 import OneSignal from "react-onesignal";
 import Home from "./components/Home";
 import Page from "./components/Page";
-import Notfound from ".components/Notfound";
+import Notfound from "./components/Notfound";
 import PWAPrompt from 'react-ios-pwa-prompt'
 OneSignal.initialize('bdd3a802-1273-48f5-96cb-ea87cd7f691d', {
   safari_web_id:"web.onesignal.auto.1947bcbb-3df5-45a5-b464-0be0e15f4a2c"
