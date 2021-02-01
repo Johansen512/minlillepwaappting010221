2d6404ae41b88c697f0547179837dca72254e195
Christian Johansen <137861@RTS-365.dk>
2021-02-01 09:16:52 +0100

    med online billede

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,7 +6,7 @@ const Page = () => {
         <h2>Dette er undersiden</h2>
 
         <img src={kode} alt="tu" />
-        
+        <img src="https://unsplash.com/photos/PubTsM3Bx4c" alt="tu" />
         </div>     );
 }
  
