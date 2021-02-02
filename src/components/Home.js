2a8dfec6cf48e75e7b15f69309b46aca176d793d
Christian Johansen <137861@RTS-365.dk>
2021-02-02 12:32:30 +0100

    pillet ved css ... igen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 /**@jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import logo from '../logo.svg';
 import '../App.css';
 import React from 'react';
@@ -6,18 +7,35 @@ import React from 'react';
 
 
 const Home = () => {
+
+const HomeStyle= css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  background-color:#332e92;
+  color: white;
+  margin: 5rem;
+  padding: 1rem;
+`;
+
     return ( 
 
 
         <div className="App">
           
       <header className="App-header">
+      
+       
+      </header>
+<section css={HomeStyle}>
       <img src={logo} className="App-logo" alt="logo" />
         <p>
           Velkommen til Fantasks mobile abonnements-service
         </p>
-       
-      </header>
+
+        </section>
     </div>
      );
 }
