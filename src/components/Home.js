93568726a13f72e4a7812aec5010b4a961d3c60c
Christian Johansen <137861@RTS-365.dk>
2021-02-02 10:58:17 +0100

    etablering, manifest og delvis serviceworker

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,23 @@
+/**@jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import logo from '../logo.svg';
 import '../App.css';
-import Demo from './Demo'
+import React from 'react';
+
+
 
 const Home = () => {
     return ( 
 
 
         <div className="App">
-          <Demo />
+          
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Velkommen til Fantasks mobile abonnements-service
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
      );
