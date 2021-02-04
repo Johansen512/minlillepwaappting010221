91a05363a748b10441b9d351032d92111e55d3b7
Christian Johansen <137861@RTS-365.dk>
2021-02-04 07:54:53 +0100

    ny side

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar"
 import Notfound from "./components/Notfound";
 import PWAPrompt from 'react-ios-pwa-prompt'
 import Anotherpage from './components/Anotherpage';
+import Hemmelig from './components/hemmelig'
 OneSignal.initialize('91c782cf-3151-4837-a221-a05ac166070f', {
   safari_web_id:"web.onesignal.auto.5d6ab6d2-4d0b-4ad8-b336-7f8ff3aff850"
 })
@@ -22,6 +23,7 @@ function App() {
     <Home path= "/" />
     <Page path="/Page"/>
     <Anotherpage path="/Anotherpage"/>
+    <Hemmelig path="/Hemmelig"/>
     <Notfound default/>
 
     </Router>
---
+++ src/components/hemmelig.js
@@ -0,0 +1,34 @@
+/**@jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import '../App.css';
+import React from 'react';
+import kode from "../img/dd.png"
+
+const Hemmelig = () => {
+
+    const PageStyling = css`
+     display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-size: 1.5rem;
+    background-color: #332e92;
+    color: white;
+    
+    
+    `;
+
+
+
+
+    return (
+<div css={PageStyling}>
+        <h2>Den hemmelige side ... Sssshhhh</h2>
+        <p>Hvem er Dupond ... og hvem er Dupont?</p>
+
+        <img src={kode} alt="tu" />
+        
+        </div>     );
+}
+ 
+export default Hemmelig;
