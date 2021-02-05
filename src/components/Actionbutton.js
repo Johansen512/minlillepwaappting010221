8d2558ef0a4ea2bacd4687aa023a8f34c391d2e6
Christian Johansen <137861@RTS-365.dk>
2021-02-05 08:57:00 +0100

    button styling

--- src/components/Actionbutton.js
+++ src/components/Actionbutton.js
@@ -5,6 +5,23 @@ import React from 'react';
 
 const Actionbutton = () => {
 
+
+
+    const actionstyle = css`
+    font-size: 1.3rem;
+    width: 8rem;
+    height: 5rem;
+    border: 8px solid #3a799d;
+    background-color: #4c93bc;
+    padding: 0.5rem;
+    margin: 0.5rem;
+    border-radius:30%;
+    box-shadow: 0.25rem 0.25rem 0.8rem;
+    outline: 0;
+    
+    `;
+
+
 function badylf (){
     console.log ("det er jo badylf")
     }
@@ -12,7 +29,7 @@ function badylf (){
     
     return ( 
 <>
-<button onClick={badylf}>TRYK HER</button>
+<button onClick={badylf} css={actionstyle}>TRYK HER</button>
 </>
 
 
